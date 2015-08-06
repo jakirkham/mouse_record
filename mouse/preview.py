1a9e0d836ff16ade4d82e6223c8460cf7143ad9f
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-06 18:53:56 +0000

    Added Preview feature.

---
+++ mouse/preview.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+import time
+import sys
+import picamera
+def main():
+    with picamera.PiCamera(framerate=90) as camera:
+        try:
+            camera.preview_fullscreen=False
+            camera.preview_window=(100,20,640,480)
+            time.sleep(2)
+            camera.start_preview()
+            time.sleep(10)
+            camera.stop_preview
+        except KeyboardInterrupt:
+            print("-ending session-")
+    return 0
+                
+    
+if __name__ == "__main__":
+   import sys
+   
+   sys.exit(main())
+
