eb0807b5a56ecc7ebc55c801a151860d26c88f05
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-06 10:23:15 -0400

    mouse/picture.py: Edited docstring, succinctified directory, dropped default command line argment.

--- mouse/picture.py
+++ mouse/picture.py
@@ -12,8 +12,7 @@ import os
 
 def main(*argv):
     """
-        Simple main function that performs event center recording.
-        Defines arguments:
+        Simple main function that takes a photo of the background when run.
 
         Args:
             argv(str):                      Arguments are stored as a list
@@ -30,21 +29,17 @@ def main(*argv):
     parser.add_argument(
         "folder",
         type=str,
-        default='.',
         help="Directory"
     )
     args = parser.parse_args(argv[1:])
     folder = args.folder
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
     with picamera.PiCamera(framerate=90) as camera:
-        try:
-            # Creates directory/folder if the desired directory is nonexistent
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-                j = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f")
-                camera.capture(folder, "background" + str(j) + ".jpg")
-
-        except KeyboardInterrupt:
-            print("Program is now ending session.")
+        j = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f")
+        camera.capture(os.path.join(
+            folder, "background" + str(j) + os.extsep + "jpg"
+        ))
 
     return 0
