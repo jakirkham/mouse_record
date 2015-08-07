f37887859a6595bada84bde57fca7c4037d971f7
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-07 16:10:19 +0000

    preview.py: added optional help/program description, reformatted , and replaced 'except' statement with 'finally' statement.

--- mouse/preview.py
+++ mouse/preview.py
@@ -1,22 +1,27 @@
-#!/usr/bin/env python
+from __future__ import print_function
 
+__author__ = "Bailey Nozomu Hwa <hwab@janelia.hhmi.org"
+__date__ = "$July 30, 2015 20:20:18 EDT$"
 
+import argparse
 import time
 import sys
 import picamera
+
+
 def main(*argv):
+    parser = argparse.ArgumentParser(
+        description="Takes camera preview. Exit using KeyboardInterrupt."
+        )
+    args =parser.parse_args()
     with picamera.PiCamera(framerate=90) as camera:
         try:
-            camera.preview_fullscreen=False
-            camera.preview_window=(100,20,640,480)
-            time.sleep(2)
-            camera.start_preview()
-            time.sleep(10)
+            while True:
+                camera.preview_fullscreen=False
+                camera.preview_window=(100,20,640,480)
+                camera.start_preview()
+                time.sleep(600)
+        finally:
             camera.stop_preview
-        except KeyboardInterrupt:
             print("-ending session-")
     return 0
-                
-    
-
-
