2f42347d92011da72e2cf560827ae565697fd39f
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-07 16:59:45 +0000

    mouse/preview.py: added argument for user to specify length of preview and edited format.

--- mouse/preview.py
+++ mouse/preview.py
@@ -13,14 +13,21 @@ def main(*argv):
     parser = argparse.ArgumentParser(
         description = "Takes camera preview. Exit using KeyboardInterrupt."
     )
+    parser.add_argument(
+        "time",
+        type = int,
+        default = "60",
+        help = "Preview length (Defaults to 60 seconds)"
+    )    
     args = parser.parse_args()
+    t = args.time
+
     with picamera.PiCamera(framerate = 90) as camera:
         try:
-            while True:
-                camera.preview_fullscreen = False
-                camera.preview_window = (100,20,640,480)
-                camera.start_preview()
-                time.sleep(600)
+            camera.preview_fullscreen = False
+            camera.preview_window = (100,20,640,480)
+            camera.start_preview()
+            time.sleep(t)
         finally:
             camera.stop_preview()
             print("-ending session-")
