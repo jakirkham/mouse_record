46d9a65d8e5a331c8d7c31a3ba6e498cea630fd5
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-07 16:43:51 +0000

    mouse/preview.py: fixed spaceing and reformatted.

--- mouse/preview.py
+++ mouse/preview.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-__author__ = "Bailey Nozomu Hwa <hwab@janelia.hhmi.org"
+__author__ = "Bailey Nozomu Hwa <hwab@janelia.hhmi.org>"
 __date__ = "$July 30, 2015 20:20:18 EDT$"
 
 import argparse
@@ -11,17 +11,17 @@ import picamera
 
 def main(*argv):
     parser = argparse.ArgumentParser(
-        description="Takes camera preview. Exit using KeyboardInterrupt."
-        )
-    args =parser.parse_args()
-    with picamera.PiCamera(framerate=90) as camera:
+        description = "Takes camera preview. Exit using KeyboardInterrupt."
+    )
+    args = parser.parse_args()
+    with picamera.PiCamera(framerate = 90) as camera:
         try:
             while True:
-                camera.preview_fullscreen=False
-                camera.preview_window=(100,20,640,480)
+                camera.preview_fullscreen = False
+                camera.preview_window = (100,20,640,480)
                 camera.start_preview()
                 time.sleep(600)
         finally:
-            camera.stop_preview
+            camera.stop_preview()
             print("-ending session-")
     return 0
