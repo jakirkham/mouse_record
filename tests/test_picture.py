131d6331c173bce66d5d5a4a20568b5c3456bbf6
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-10 14:59:19 -0400

    tests/: added tests for picture.py and preview.py.

---
+++ tests/test_picture.py
@@ -0,0 +1,52 @@
+__author__ = "Bailey Nozomu Hwa <hwab@janelia.hhmi.org>"
+__date__ = "$Aug 10, 2015, 2:38 PM$"
+
+import os
+import shutil
+import tempfile
+import time
+
+from mouse.picture import main
+
+
+class TestPicture(object):
+    def setup(self):
+        self.cwd = os.getcwd()
+        self.tempdir = ""
+        self.tempdir = tempfile.mkdtemp()
+        os.chdir(self.tempdir)
+
+        print("tempdir = \"%s\"" % self.tempdir)
+
+    def teardown(self):
+        os.chdir(self.cwd)
+        shutil.rmtree(self.tempdir)
+        self.tempdir = ""
+        self.cwd = ""
+
+    def test_main_0(self):
+        main("",
+             "."
+        )
+
+        while len(os.listdir(self.tempdir)) < 2:
+           time.sleep(1)
+        time.sleep(1)
+
+        filenames = []
+        for each_filename in os.listdir(self.tempdir):
+            filenames.append(os.path.join(self.tempdir, each_filename))
+        filenames.sort()
+
+        assert ".err" in filenames[0]
+        assert ".out" in filenames[1]
+
+        with open(filenames[0], "r") as f:
+            s = f.read().strip()
+            print("File \"%s\" contains \"%s\"." % (f.name, s))
+            assert s == ""
+
+        with open(filenames[1], "r") as f:
+            s = f.read().strip()
+            print("File \"%s\" contains \"%s\"." % (f.name, s))
+            assert s == "output"
