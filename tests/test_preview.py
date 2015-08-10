4162acd7a8fdea046aaf0208c77ae20b0a1719fd
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-10 15:54:43 -0400

    tests/test_preview.py:removed unused import.

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -1,11 +1,6 @@
 __author__ = "Bailey Nozomu Hwa <hwab@janelia.hhmi.org>"
 __date__ = "$Aug 10, 2015, 2:38 PM$"
 
-import os
-import shutil
-import tempfile
-import time
-
 from mouse.preview import main
 class TestPreview(object):
     def test_main_0(self):
