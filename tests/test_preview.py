dc94e904dbc819bd27d2347f8721c2e866881bdc
Bailey Nozomu Hwa <hwab@janelia.hhmi.org>
2015-08-10 19:23:25 +0000

    tests/test_preview.py: dropped unnecessary teardown and setup. Also reformatted spacing.

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -7,28 +7,9 @@ import tempfile
 import time
 
 from mouse.preview import main
-
-
 class TestPreview(object):
-    def setup(self):
-        self.cwd = os.getcwd()
-        self.tempdir = ""
-        self.tempdir = tempfile.mkdtemp()
-        os.chdir(self.tempdir)
-
-        print("tempdir = \"%s\"" % self.tempdir)
-
-    def teardown(self):
-        os.chdir(self.cwd)
-        shutil.rmtree(self.tempdir)
-        self.tempdir = ""
-        self.cwd = ""
-
     def test_main_0(self):
-        main("",
-             "1"
+        main(
+            "",
+            "1"
         )
-
-        while len(os.listdir(self.tempdir)) < 2:
-           time.sleep(1)
-        time.sleep(1)
