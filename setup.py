b700b62bd584238645d25e1cbe3e37c539eea63f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-14 17:54:35 -0400

    setup.py: Edited to use versioneer.

--- setup.py
+++ setup.py
@@ -6,17 +6,21 @@ __date__ = "$July 30, 2015 20:20:18 EDT$"
 from glob import glob
 from setuptools import setup, find_packages
 
+import versioneer
+
 
 setup(
     name="mouse_recorder",
-    version="0.0.1",
+    version=versioneer.get_version(),
     description="An Event Triggered Recorder",
     url="https://github.com/DudLab/mouse_record",
     license="GPLv3",
     author="Bailey Hwa",
     author_email="hwab@janelia.hhmi.org",
     scripts=glob("bin/*"),
+    py_modules=["versioneer"],
     packages=find_packages(exclude=["tests*"]),
+    cmdclass=versioneer.get_cmdclass(),
     install_requires=["picamera >=1.0", "RPi.GPIO >=0.5.11"],
     tests_require=["nose", "mock"],
     test_suite="nose.collector",
