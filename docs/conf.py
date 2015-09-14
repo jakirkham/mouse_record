8c0008aa54f79bfcdf7bf80a3f7f910aa808a925
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-14 15:45:07 -0400

    docs/conf.py: Add author.

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'mouse_recorder'
 copyright = u'2015, Bailey Hwa'
+author = u'Bailey Hwa'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
