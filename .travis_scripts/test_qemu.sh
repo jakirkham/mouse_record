58f197adaca360e8e41a7fdd0a3b66aabafd1523
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-15 00:21:42 -0400

    .travis_scripts/test_qemu.sh: Export `$LIBRARY_PATH` and the run the tests.

--- .travis_scripts/test_qemu.sh
+++ .travis_scripts/test_qemu.sh
@@ -15,7 +15,7 @@ GUEST_DEPENDENCIES="sudo curl python python-dev git"
 
 # Command used to run the tests
 # Try to pick up libmmal.so for picamera by specifying this path.
-TEST_COMMAND="LIBRARY_PATH=/opt/vc/lib python setup.py test"
+TEST_COMMAND="export LIBRARY_PATH=/opt/vc/lib && python setup.py test"
 
 function setup_arm_chroot {
     # Host dependencies
